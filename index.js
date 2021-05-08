a3a76d2a833688f7fcededb5693dee5949a06fe6
Mauricio Filho <mauricio.filho.contato@gmail.com>
2021-05-08 11:43:49 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log("hello world");
+
+var nomevariavel= "kim boila"
+
+nomevariavel= "maurissola"
+
+console.log(nomevariavel);
+
+var x= 1
+var y= 2
+console.log(x+y);
+
+var a= 2021
+var q= 1995 
+console.log(a-q);
+
+function multiplicardois (valor1, valor2){
+    return valor1 * valor2
+}
+
+console.log (multiplicardois(2, 5));
+
+
+
+
+
+
+function teoremapitagorasA (catetoa, catetob){
+    return (Math.sqrt((catetoa * catetoa) + (catetob * catetob)))
+}
+console.log (teoremapitagorasA(24, 28));
+
+
---
+++ pitagoras.js
@@ -0,0 +1,6 @@
+
+function teoremapitagoras (catetoa, catetob){
+   
+    return catetoa + catetob;
+
+    Console.log (teoremapitagoas(21, 28))
